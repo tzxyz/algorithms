abbc4b02d30623d4f440d587c9fe4c49527cead6
zhuonima <tz.zhuo@protonmail.com>
2017-09-04 23:00:39 +0800

    fix quick sort bug & add tests

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -1,39 +1,48 @@
 package sort;
 
-import java.util.Arrays;
-
 public class QuickSort {
 
     public static <T extends Comparable<T>> void quickSort(T[] elements) {
-        pivot(elements);
+        quickSort(elements, 0, elements.length - 1);
     }
 
-    public static <T extends Comparable<T>> void quickSort(T[] elements, int start, int end) {
-        if (elements.length < 2) return;
-//        elements
+    private static <T extends Comparable<T>> void quickSort(T[] elements, int start, int end) {
+
+        if (end - start <= 1) return;
+        T pivot = pivot(elements, start, end);
+        int i = start, j = end - 1;
+
+        for (;;) {
+            while (elements[i].compareTo(pivot) < 0) i++;
+            while (elements[j].compareTo(pivot) > 0) j--;
+            if (i < j) swap(elements, i, j);
+            else break;
+        }
+        swap(elements, i, end);
+        quickSort(elements, start, i - 1);
+        quickSort(elements, i + 1, end);
     }
 
-    private static <T extends Comparable<T>> void pivot(T[] element) {
-        int start = 0;
-        int end = element.length - 1;
-        int mid = (end - start) / 2;
+    private static <T extends Comparable<T>> T pivot(T[] element, int start, int end) {
 
-        T left = element[0];
-        T middle = element[mid];
-        T right = element[end];
+        int center = (start + end) / 2;
 
-        if (left.compareTo(middle) > 0) {
-            T tmp = left;
-            left = middle;
-            middle = tmp;
-        }
+        T left = element[0], middle = element[center], right = element[end];
 
-        if (middle.compareTo(right) < 0) {
-            T tmp = middle;
-            middle = right;
-            right = tmp;
-        }
+        if (left.compareTo(middle) > 0)  swap(element, start, center);
 
-        System.out.println(String.format("start: %s, middle: %s, end: %s, elements: %s", left, middle, right, Arrays.toString(element)));
+        if (left.compareTo(right) > 0)   swap(element, start, end);
+
+        if (middle.compareTo(right) > 0) swap(element, center, end);
+
+        swap(element, center, end);
+
+        return element[end];
+    }
+
+    private static <T> void swap(T[] array, int i, int j) {
+        T t = array[i];
+        array[i] = array[j];
+        array[j] = t;
     }
 }
--- src/test/java/sort/QuickSortTest.java
+++ src/test/java/sort/QuickSortTest.java
@@ -1,10 +1,10 @@
 package sort;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,16 +12,20 @@ public class QuickSortTest {
 
     private Integer[] ints = new Integer[10];
 
-    @Before
-    public void init() {
-        IntStream.range(0, 10).map(i -> new Random().nextInt(100)).boxed().collect(Collectors.toList()).toArray(ints);
-    }
+    private BiFunction<Integer, Integer, Integer[]> function = (Integer range, Integer bound) -> IntStream.range(0, range).map(i -> new Random().nextInt(bound)).boxed().collect(Collectors.toList()).toArray(ints);
 
     @Test
     public void quickSortTest() {
-        System.out.println(Arrays.toString(ints));
-        QuickSort.quickSort(ints);
-        System.out.println(Arrays.toString(ints));
+
+        for (int i = 0; i<= 1000; i ++) {
+            function.apply(10, 100);
+            System.out.println(String.format("排序前: %s", Arrays.toString(ints)));
+            QuickSort.quickSort(ints);
+            System.out.println(String.format("排序后: %s", Arrays.toString(ints)));
+            System.out.println();
+        }
+
+
     }
 
 }
